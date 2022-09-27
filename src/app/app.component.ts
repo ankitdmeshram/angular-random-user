04944af18e656c7768dab25bb5a395d7fa457565
Ankit Meshram <ankit@brokod.com>
2022-09-27 17:12:43 +0530

    first commit

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,10 +1,34 @@
-import { Component } from '@angular/core';
-
+import { Component, OnInit } from '@angular/core';
+import { ToastrService } from "ngx-toastr"
+import { UserService } from './services/user.service';
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.css']
 })
-export class AppComponent {
+
+export class AppComponent implements OnInit {
   title = 'random-card';
+
+  user: any;
+
+  constructor(
+    private userService: UserService,
+    private toastr: ToastrService
+    ) { }
+
+  ngOnInit() {
+    this.userService.getUser().subscribe(
+      (res: any) => {
+        console.log(res);
+        console.log("+++++++")
+        this.user = res.results[0];
+        console.log(this.user);
+      },
+      (err) => {
+        this.toastr.error(err.status, "Oops");
+      }
+    )
+  }
+
 }
